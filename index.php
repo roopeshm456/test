0f6e1d870a5731cc024c62821e618314b3e5630a
roopeshm456 <46523847+roopeshm456@users.noreply.github.com>
2020-03-06 17:28:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-echo "abc";
+if($_POST['submit']){
+
+	$to = $_POST['email'];
+	$subject="Caterplace Greeting";
+
+
+	$headers  = "From: info@caterplace.com\n";
+	$headers .= "X-Mailer: PHP/" . phpversion()."\n"; // mailer
+	$headers .= "Content-Type: text/html; charset=iso-8859-1\n";
+
+
+	$mail_body = "Hi, This is  a test PHP mail from caterplace";
+
+	echo mail($to,$subject,$mail_body,$headers);
+}
+
+?>
+
+
+<html>
+<form method="POST">
+<input type="email" name="email" placeholder="enter your email address" />
+<input type="submit"  name="submit"/>
+</form>
+</html>
